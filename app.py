a82847aa4756210de117a800bf73ce383e3cf220
Mo Mobarak <mo@mobarak.com>
2025-01-11 18:56:34 +0000

    configure the camera

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
+from picamera2 import Picamera2
+from libcamera import Transform
+
+# Initialize the camera
+camera = Picamera2()
+camera.configure(camera.create_video_configuration(transform=Transform(vflip=True), main={"size": (640, 480)}))
+camera.set_controls({"AwbEnable": True})  # Enable auto white balance
+camera.start()
+
 def main():
     print("Hello from pi-video-streamer!")
 
